69f8f705b4a48591934d9f7e4b0550afaf732cda
Elcale1703 <m.agudeloo@uniandes.edu.co>
2025-05-10 11:43:24 -0500

    Completo .env si algo

--- src/client/client.js
+++ src/client/client.js
@@ -5,6 +5,6 @@ export const client = sanityClient({
     dataset: 'production',
     apiVersion: '2022-03-07',
     useCdn: true,
-    token: 'sk7GLCdP6rgyd9x6vko2m5g9IC2tz01QL8e4Vq4zMHfWtAbOpK3bfOud6nihZSfwcPw9OsdVbu0XtNK0rz8XFyedLWqOkEjaZfaGYdF5YY8hl6lxkf2v9EhbNAhuGMSoxRKPdQmIQ6Ff64VZDucS3vVGj1dysxrCQLE7IE7vUeJYlc2fR9wv'
+    token: process.env.REACT_APP_SANITY_TOKEN
 });
 
