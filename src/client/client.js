bf8e05cb0e2d1be3a39bcdd1ec2187bbbe4bc412
Elcale1703 <m.agudeloo@uniandes.edu.co>
2025-05-12 13:20:29 -0500

    Listo builder de imagenes

--- src/client/client.js
+++ src/client/client.js
@@ -1,4 +1,5 @@
 import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
     projectId: '03g3l6a8',
@@ -8,3 +9,17 @@ export const client = sanityClient({
     token: process.env.REACT_APP_SANITY_TOKEN
 });
 
+const builder = imageUrlBuilder(client);
+export const urlFor = (source) => {
+    const ref = source.asset._ref;
+
+    if (source._type === 'image') {
+        return builder.image(source).url();
+    } else if (source._type === 'file') {
+        const [, id, extension] = ref.split('-');
+        return `https://cdn.sanity.io/files/${client.config().projectId}/${client.config().dataset}/${id}.${extension}`;
+    } else if (source._type === 'video') {
+        const [, id, extension] = ref.split('-'); 
+        return `https://cdn.sanity.io/files/${client.config().projectId}/${client.config().dataset}/${id}.${extension}`;
+    }
+};
